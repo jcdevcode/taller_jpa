1c579adce205fc97ce2d5135a60449087d4c750b
Julio Cesar Bolaños Palacios <cesar.dev.code@gmail.com>
2017-05-23 01:31:56 -0500

    Se agrega primera entidad y test unitario.

---
+++ jpa-examples/src/main/java/mx/bluecode/model/entities/Persona.java
@@ -0,0 +1,92 @@
+package mx.bluecode.model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_PERSONAS")
+public class Persona implements Serializable{	
+	
+	private static final long serialVersionUID = 3537020748450083556L;
+
+	@Id
+	@Column(name="ID_EMPLEADO")
+	private Long id;
+	
+	@Column(name="NOMBRE")
+	private String nombre;
+	
+	@Column(name="APELLIDOS")
+	private String apellidos;
+	
+	@Column(name="FECHA_NACIMIENTO")
+	private Date fechaNacimiento;	
+	
+	
+	public Persona(){
+		super();
+	}
+	
+	public Persona(Long id, String nombre, String apellidos, Date fechaNacimiento) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", fechaNacimiento="
+				+ fechaNacimiento + "]";
+	}
+
+	
+	
+}
